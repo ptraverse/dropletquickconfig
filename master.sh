5eb16be6119ea9c45c93b72fd9e0f30996658ae8
Philippe Traverse <philippe.traverse@gmail.com>
2017-01-23 08:46:56 +0000

    install yarn

--- master.sh
+++ master.sh
@@ -53,6 +53,7 @@ sudo apt-get install -y nodejs --allow-unauthenticated
 # Yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn -y
 
 # Mongo
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
